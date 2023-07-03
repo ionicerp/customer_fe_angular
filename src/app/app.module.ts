ef51a470d436e1e48810af7ffd4a796b24e92794
Vincent Tan <vincenttan83@live.com>
2023-07-03 14:04:32 +0800

    integrated auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'ionicerp.us.auth0.com',
+      clientId: 'aenDxKSGvLmuiK2Ss5LZLNopi623qwel',
+      authorizationParams: {
+        redirect_uri: `${window.location.origin}/customer`,
+        prompt: 'login'
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
